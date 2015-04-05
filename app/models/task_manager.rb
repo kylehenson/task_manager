eda2f2a9434451c2fedf3b929659c735ee02aa5c
Kyle Henson <khenson.kh@gmail.com>
2015-04-04 18:02:25 -0600

    Refactored

--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -1,4 +1,3 @@
-# require 'yaml/store'
 require 'sequel'
 require 'sqlite3'
 
@@ -6,10 +5,8 @@ class TaskManager
 
   def self.database
     if ENV["TASK_MANAGER_ENV"] == 'test'
-      # @database ||= YAML::Store.new("db/task_manager_test")
       @database ||= Sequel.sqlite("db/task_manager_test.sqlite3")
     else
-      # @database ||= YAML::Store.new("db/task_manager")
       @database ||= Sequel.sqlite("db/task_manager.sqlite3")
     end
   end
@@ -17,12 +14,6 @@ class TaskManager
   def self.create(task)
     data = {"title" => task[:title], "description" => task[:description]}
     database.from(:tasks).insert(data)
-    # database.transaction do
-    #   database['tasks'] ||= []
-    #   database['total'] ||= 0
-    #   database['total'] += 1
-    #   database['tasks'] << { "id" => database['total'], "title" => task[:title], "description" => task[:description] }
-    # end
   end
 
   def self.raw_tasks
@@ -35,7 +26,6 @@ class TaskManager
     database.from(:tasks).all.map do |raw_task|
       Task.new(raw_task)
     end
-    # raw_tasks.map { |data| Task.new(data) }
   end
 
   def self.raw_task(id)
@@ -48,18 +38,10 @@ class TaskManager
 
   def self.update(id, task)
     database.from(:tasks).where(:id => id).update(title: task[:title], description: task[:description])
-    # database.transaction do
-    #   target = database['tasks'].find { |data| data["id"] == id }
-    #   target["title"] = task[:title]
-    #   target["description"] = task[:description]
-    # end
   end
 
   def self.destroy(id)
-    # database.transaction do
-      # database['tasks'].delete_if { |task| task["id"] == id }
     database.from(:tasks).where(:id => id).delete
-    # end
   end
 
   def self.delete_all
